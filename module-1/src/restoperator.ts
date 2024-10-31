6250f1c9f3b400348cf91620d2c1fa11a07219dd
Sanchita Devi <thesanchitadevi@gmail.com>
2024-10-31 19:39:15 +0600

    rest operator

--- module-1/src/restoperator.ts
+++ module-1/src/restoperator.ts
@@ -1,1 +1,13 @@
 // Rest Opersator
+
+const students =(...names: string[]) => {
+    // console.log(`Hello ${names.join(', ')}`);
+
+    names.forEach((name: string) => {
+        console.log(`Hello ${names.join(', ')}`);
+    });
+}
+
+students('John', 'Jane',  'James', 'Jill');
+
+//output: Hello John, Jane, James, Jill
